b59247eac7506b4bd1c33f4a30d13cbf596150ba
Tatsuki Terao <tterao@c1r14s3.42tokyo.jp>
2023-11-01 19:44:09 +0900

    ex02 init

---
+++ ex02/test/identify_test.cc
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include <gtest/internal/gtest-port.h>
+#include "gtest/gtest-spi.h"
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+TEST(Identify_test, identify) {
+
+	
+
+
+}
